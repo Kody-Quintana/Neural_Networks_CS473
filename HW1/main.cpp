38dac16e682e23ff87092db3c65b89c3f4bfdaf6
Kody.Quintana <Quintana.Kody@gmail.com>
2019-01-23 16:05:20 -0700

    Add HW1

---
+++ HW1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace dp{//Create a namespace to allow use of "Dot Product" operator on std:vector
+	template<class T>
+	T operator*(vector<T> first, vector<T> second){
+		T temp = 0;
+		for (int i = 0; i < first.size(); ++i){
+			temp = first[i] * second[i];
+		}
+		return( temp );
+	}
+}
+
+int main( int argc, char** argv ){
+	vector<float> W{ 0.5, 0.3, 0.8 };
+	vector<float> X{ 1.3, 1.7, 4.5 };
+
+	using namespace dp;
+	cout << W * X << endl;
+	return(0);
+}
