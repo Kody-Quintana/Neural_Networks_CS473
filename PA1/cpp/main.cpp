9a7a3ccc867ff2ffdb0a88672d029610b6e877f6
Kody Quintana <Quintana.Kody@gmail.com>
2019-03-05 22:43:24 -0700

    Change to vector of vector pointers for inputs
    Start over on implementation

--- PA1/cpp/main.cpp
+++ PA1/cpp/main.cpp
@@ -2,29 +2,72 @@
 #include <vector>
 #include <cmath>
 #include <memory>
+#include <random>
+
 #include "dotproduct.hpp"
 #include "neuralnet.hpp"
 
-using namespace dp;
+#define INSTANCES_SIZE 15
+#define INPUTS_SIZE 10
 
+using namespace dp;
 using namespace std;
 
 int main( int argc, char** argv ){
 
-	vector<double> X{ 10, 10, 10, 1, 
-	                  0.5, 0.3, 0.6, 0.8, 
-			  0.4, 1.0, 0.3, 0.9};
-	vector<double> Y{ 1.3, 1.7, 3.2, 4.5 };
-
-	//{
-	//	using dp::operator*;
-	//	cout << W*X << endl;
-	//}
 
 
-	unique_ptr<NeuralNet<double>> net( new NeuralNet<double>);
-	//NeuralNet<double>* net = new NeuralNet<double>();
-	net->train(X, Y, 10000);
+	//Random double generator
+	double lower_bound = 0.0;
+	double upper_bound = 1.0;
+	std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
+	std::default_random_engine re;
+
+
+
+	//Vector of vector pointers to avoid a vector of vectors
+	vector< vector<double>* > Inputs;
+	Inputs.reserve(INPUTS_SIZE + 1); //Add one slot for the bias ones
+	vector<double>* x = new vector<double>(INSTANCES_SIZE, 1.0); //Bias ones
+	Inputs.emplace_back(x);//Set first input to ones for use with the bias
+
+	//Fill the rest of the inputs with vectors of random numbers between 1.0 and 0.0
+	for (int i = 0; i < INPUTS_SIZE; ++i){
+		x = new vector<double>;
+		x->reserve(INSTANCES_SIZE);
+		Inputs.emplace_back(x);
+	}
+
+	//Fill with random doubles, skip first input because its zeros for the bias
+	for (int i = 1; i < INPUTS_SIZE + 1; ++i){
+		for (int j = 0; j < INSTANCES_SIZE; ++j){
+			Inputs[i]->emplace_back(unif(re));
+		}
+	}
+
+
+	//Test print
+	for (int i = 0; i < Inputs.size(); ++i){
+		cout << "\n\nINPUT# " << i << "\n";
+		for (int j = 0; j < Inputs[0]->size(); ++j){
+			cout << (*Inputs[i])[j] << endl;
+		}
+	}
+
+	//Generate labels
+	vector<double> Labels;
+	Labels.reserve(INSTANCES_SIZE);
+	for (int i = 0; i < INSTANCES_SIZE; ++i){
+		Labels.emplace_back(unif(re));
+	}
+
+
+	//unique_ptr<NeuralNet<double>> net( new NeuralNet<double>);
+	//net->train(Inputs, Y, 10000);
 	
+	//Clean up inputs
+	for (auto x : Inputs){
+		delete x;
+	}
 	return(0);
 }
--- PA1/cpp/neuralnet.hpp
+++ PA1/cpp/neuralnet.hpp
@@ -3,174 +3,62 @@
 #include <memory>
 #include <array>
 #include <iostream>
-//#include <ios>
+#include <random>
+
+#include "dotproduct.hpp"
 
 using std::vector;
 using std::unique_ptr;
 using std::cout;
 using std::endl;
 
+
 template<class T>
 class NeuralNet{
 	public:
-		NeuralNet();
-		~NeuralNet();
-		void train(vector<T> inputs, vector<T> labels, int iterations);
-		vector<T> run();
+	//	NeuralNet();
+	//	~NeuralNet();
+		void train( vector<vector<T>*> Inputs, vector<T> Labels );
 	private:
-		T activation(T input);
-		T activation_derivative(T input);
-		T one = 1.0; //Not sure if this is the right way to specify precision
-		vector<T> weights;
-		T learning_rate = 0.0001;
-		T rmse(vector<T> input);
-		vector<T> labels;
-
+		vector< vector<T>* > Inputs;
+		vector<T> Labels;
+		vector<T> Weights;
+		T y_hat(int instance);
+		T error(int instance);
 };
 
 
 
-
-
+template<class T>
+T NeuralNet<T>::y_hat(int instance){
+	T y_hat_value = 0.0;
+	for (int i = 0; i < Weights.size(); ++i)
+		y_hat_value += Weights[i] * (*Inputs[i])[instance];
+	return y_hat_value;
+}
 
 
 
 template<class T>
-NeuralNet<T>::NeuralNet(){
-	cout << "NN constructed" << endl;
+T NeuralNet<T>::error(int instance){
+	return (Labels[instance] - y_hat(instance));
 }
 
-template<class T>
-NeuralNet<T>::~NeuralNet(){
-	cout << "NN destructed" << endl;
-}
 
 
 template<class T>
-void NeuralNet<T>::train(vector<T> inputs, vector<T> target_labels, int iterations){
-	labels = target_labels;
+void NeuralNet<T>::train( vector<vector<T>* > arg_inputs, vector<T> arg_labels ){
+	Inputs = arg_inputs;
+	Labels = arg_labels;
 
-	//2D X matrix is stored in 1D std::vector
-	int instances = labels.size(); //Number of x instances should always match the number of y label instances
-	int number_of_inputs = inputs.size() / labels.size();
-	cout << "Number of inputs: " << number_of_inputs << endl;
-	cout << "Number of instances: " << instances << endl;
 
-	//Normalize labels
-	T label_min = labels[0];
-	T label_max = labels[0];
-	for (int i = 1; i < labels.size() ; ++i){
-		label_min = std::min(label_min, labels[i]);
-		label_max = std::max(label_max, labels[i]);
+	//Initialize Weights with random values
+	Weights.reserve(Inputs.size());
+	std::uniform_real_distribution<double> unif(0.1, 10.0);
+	std::default_random_engine re;
+	for (int i = 0; i < Inputs.size(); ++i){
+		Weights.emplace_back(unif(re));
 	}
-	for (int i = 0; i < labels.size() ; ++i){
-		labels[i] = (labels[i] - label_min) / (label_max - label_min);
-	}
-
-	//1 - 0 encode the y labels
-	for (int i = 0; i < labels.size(); ++i){
-		if (labels[i] < 0.5) labels[i] = 0.0;
-		else labels[i] = 1;
-	}
-	cout << "Encoded labels: " << endl;
-	for (auto i : labels){
-		cout << i << endl;
-	}
-
-	//Normalize inputs
-	T inputs_min = inputs[0];
-	T inputs_max = inputs[0];
-	for (int i = 1; i < inputs.size() ; ++i){
-		inputs_min = std::min(inputs_min, inputs[i]);
-		inputs_max = std::max(inputs_max, inputs[i]);
-	}
-	for (int i = 0; i < inputs.size() ; ++i){
-		inputs[i] = (inputs[i] - inputs_min) / (inputs_max - inputs_min);
-	}
-
-
-	//Use of error here is only to adjust weights with higher errors by more than the other weights
-	//vector<T> error;
-	//error.reserve( labels.size() );
-
-	//One weight per dimension, plus one bias weight
-	weights.reserve( number_of_inputs + 1 );
-	//weights( labels.size() + 1, 0.0 );
-	for (int i = 0; i < (number_of_inputs + 1); ++i){
-		weights.emplace_back( 0.0 );
-	}
-
-	//This could also be a while RMSE is greater than something loop
-	for (int i = 0; i < iterations; ++i){
-		T temp_bias_adjustment = 0.0;
-
-		//Iterate through each input
-		for (int current_input = 0; current_input < number_of_inputs; ++current_input){
-
-			//Reset gradients at start of new input
-			T b_gradient = 0.0;
-			T m_gradient = 0.0;
-
-			const T m = weights[current_input + 1];
-			const T b = weights[0];
-
-			//Iterate through each instance
-			for (int instance = 0; instance < labels.size(); ++instance){
-
-				const T this_x = inputs[ (current_input * instances) + instance ];
-				const T y = labels[instance];
-				
-				//Intercept gradient is the derivative of the cost function with respect to b.
-				b_gradient += activation_derivative( -(2.0 / instances) * (y - ( m * this_x + b ) ));
-
-				//Slope gradient is the derivative of the cost function with respect to m.
-				m_gradient += activation_derivative(-(2.0 / instances) * (this_x) * (y - ( m * this_x + b) ));
-
-			}//End instances loop
-
-			//Adjust bias weight (b)
-			//Divide adjustment by number of inputs so each input has equal influnce over the shared bias
-			temp_bias_adjustment -= (learning_rate * b_gradient) / number_of_inputs;
-			//Store this adjustment and only update once per epoch (outermost loop)
-
-			//Adjust slope weight (m)
-			weights[current_input + 1] -= (learning_rate * m_gradient);
-
-		}//End inputs loop
-		weights[0] = temp_bias_adjustment;
-
-	}//End epochs loop
-	for (auto i : weights){
-		cout << std::fixed << i << endl;
-	}
-}
-
-
-template <class T>
-vector<T> NeuralNet<T>::run(){
-
-}
-
-
-template <class T>
-T NeuralNet<T>::activation(T input){
-	return one / (one + exp( -input ));
-}
-
-
-template <class T>
-T NeuralNet<T>::activation_derivative(T input){
-	return input * (one - input);
-}
-
-
-template <class T>
-T NeuralNet<T>::rmse(vector<T> input){
-	T sum = 0.0;
-	for (T error : input ){
-		sum += pow( error, 2 );
-	}
-	return sqrt( sum );
 }
 
 
